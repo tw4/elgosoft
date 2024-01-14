6f7cc6ebd7e5eb7020854bb1e2f663f561bb855a
mertsabinov <88425310+mertsabinov@users.noreply.github.com>
2024-01-14 18:45:16 +0300

    add: blog link

--- app/components/Navbar/Navbar.tsx
+++ app/components/Navbar/Navbar.tsx
@@ -1,58 +1,61 @@
 "use client";
 
-import { useState } from "react";
+import {useState} from "react";
 import styles from "./Navbar.module.css";
 
 export const Navbar = () => {
-  const [isMenuOpen, setIsMenuOpen] = useState(false);
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
 
-  const toggleMenu = () => {
-    setIsMenuOpen(!isMenuOpen);
-  };
+    const toggleMenu = () => {
+        setIsMenuOpen(!isMenuOpen);
+    };
 
-  return (
-    <nav className={styles.navbarContainer}>
-      <a href="/">
-        <img
-          className={styles.navbarLogo}
-          src="/assets/logo.png"
-          width={100}
-          alt="logo"
-        />
-      </a>
-      <ul
-        className={
-          isMenuOpen
-            ? styles.navbarItemContainerActive
-            : styles.navbarItemContainer
-        }
-      >
-        <li>
-          <a href="#home">Ana Sayfa</a>
-        </li>
-        <li>
-          <a href="#services">Hizmetler</a>
-        </li>
-        <li>
-          <a href="#about">Hakkımızda</a>
-        </li>
-        <li>
-          <a href="#experiences">Deneyim</a>
-        </li>
-        <li>
-          <a href="#contact">İletişim</a>
-        </li>
-      </ul>
-      <div
-        className={
-          isMenuOpen ? styles.hamburgerMenuActive : styles.hamburgerMenu
-        }
-        onClick={toggleMenu}
-      >
-        <div className={styles.hamburgerLine} />
-        <div className={styles.hamburgerLine} />
-        <div className={styles.hamburgerLine} />
-      </div>
-    </nav>
-  );
+    return (
+        <nav className={styles.navbarContainer}>
+            <a href="/">
+                <img
+                    className={styles.navbarLogo}
+                    src="/assets/logo.png"
+                    width={100}
+                    alt="logo"
+                />
+            </a>
+            <ul
+                className={
+                    isMenuOpen
+                        ? styles.navbarItemContainerActive
+                        : styles.navbarItemContainer
+                }
+            >
+                <li>
+                    <a href="#home">Ana Sayfa</a>
+                </li>
+                <li>
+                    <a href="#services">Hizmetler</a>
+                </li>
+                <li>
+                    <a href="#about">Hakkımızda</a>
+                </li>
+                <li>
+                    <a href="#experiences">Deneyim</a>
+                </li>
+                <li>
+                    <a href='https://blog.elgosoft.com/'>Blog</a>
+                </li>
+                <li>
+                    <a href="#contact">İletişim</a>
+                </li>
+            </ul>
+            <div
+                className={
+                    isMenuOpen ? styles.hamburgerMenuActive : styles.hamburgerMenu
+                }
+                onClick={toggleMenu}
+            >
+                <div className={styles.hamburgerLine}/>
+                <div className={styles.hamburgerLine}/>
+                <div className={styles.hamburgerLine}/>
+            </div>
+        </nav>
+    );
 };
